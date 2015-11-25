ce8ec38b27a7732ed82ea6426c64d233ace9b000
eteubert <eric@ericteubert.de>
2015-11-25 11:41:20 +0100

    automatically bump version, fix style.css update

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,77 @@
 var gulp = require('gulp'),
+    bump = require('gulp-bump');
     shell = require('gulp-shell'),
     prompt = require('gulp-prompt'),
-    wp = require('gulp-wp-file-header')(),
-    p = require('./package.json');
+    runSequence = require('run-sequence'),
+    wp = require('gulp-wp-file-header')()
+;
 
+var knownOptions = {
+  releaseType: 'patch',
+  alias: { 'r': 'releaseType' }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+function getPackageJsonVersion() {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+}
 
 gulp.task('git-dist-deploy', function() {
-    gulp.src('/')
-      .pipe(prompt.prompt([{
-        type: 'confirm',
-        name: 'task',
-        message: 'This will deploy to the Dist Branch. It auto commits and pushes to the master. Sure?'
-      },
-      {
-        type: 'confirm',
-        name: 'version',
-        message: 'Have you setup the current release version in package.json?'
-      }],
-       function(res) {
-        if (res.task) {
-            if(res.version == false){
-              console.log("go figure!");
-            }
-            else{
-              //Run the build and deploy task
-              gulp.start('deploy-cmd');
-              //Patch the style.css to the current version from package.json
-              wp.patch();
-            }
+  gulp.src('/')
+    .pipe(prompt.prompt([{
+      type: 'confirm',
+      name: 'task',
+      message: 'This will deploy to the Dist Branch. It auto commits and pushes to the master. Sure?'
+    }],
+     function(res) {
+      runSequence(
+        'bump-version',
+        'update-wp-style-css',
+        'deploy-cmd',
+        function (error) {
+          if (error) {
+            console.log(error.message);
+          } else {
+            console.log('RELEASE FINISHED SUCCESSFULLY: ' + getPackageJsonVersion());
+          }
+          callback(error);
+        });
+      //Patch the style.css to the current version from package.json
+      wp.patch();
+    }));
+});
 
+gulp.task('update-wp-style-css', function(cb) {
+  wp.patch();
+  cb();
+});
 
-        } else {
-          console.log('Ok. Go do something else first.');
-        }
-      }));
-  });
+gulp.task('bump-version', function() {
+  return gulp.src('./package.json')
+  .pipe(bump({type: options.releaseType}))
+  .pipe(gulp.dest('./'));
+});
 
-  gulp.task('deploy-cmd', shell.task([
-    'git checkout master',
-    'git add --all',
-    'git commit -m "Auto-Commit for deployment "'+ p.version,
-    'git tag -a '+ p.version + '-dev -m "Version' + p.version + '"',
-    'git push origin master ' + p.version + '-dev',
-    'git checkout -B dist',
-    'rm .gitignore',
-    'mv .gitignore-dist .gitignore',
-    'git rm -r --cached .',
-    'git add --all',
-    'git commit -m "build for release version "' + p.version,
-    'git tag -a '+ p.version + '-dist -m "Version' + p.version + '"',
-    'git push --force origin dist ' + p.version + '-dist',
-    'git checkout master',
-    'git branch -D dist',
-    'echo "Deployed Version: "' + p.version
-  ], {
-    ignoreErrors: true
-  }));
+gulp.task('deploy-cmd', shell.task([
+  'git checkout master',
+  'git add --all',
+  'git commit -m "Auto-Commit for deployment "'+ getPackageJsonVersion(),
+  'git tag -a '+ getPackageJsonVersion() + '-dev -m "Version' + getPackageJsonVersion() + '"',
+  'git push origin master ' + getPackageJsonVersion() + '-dev',
+  'git checkout -B dist',
+  'rm .gitignore',
+  'mv .gitignore-dist .gitignore',
+  'git rm -r --cached .',
+  'git add --all',
+  'git commit -m "build for release version "' + getPackageJsonVersion(),
+  'git tag -a '+ getPackageJsonVersion() + '-dist -m "Version' + getPackageJsonVersion() + '"',
+  'git push --force origin dist ' + getPackageJsonVersion() + '-dist',
+  'git checkout master',
+  'git branch -D dist',
+  'echo "Deployed Version: "' + getPackageJsonVersion()
+], {
+  ignoreErrors: true
+}));
 
 gulp.task('default');
